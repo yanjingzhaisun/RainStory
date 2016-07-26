9582df82926972de74c6fec1d4f89d3d1abdb295
Euphonux <zz1322@nyu.edu>
2016-07-25 22:34:08 -0400

    changed into c4d

---
+++ Assets/Scripts/LevelBlinker.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelBlinker : MonoBehaviour
+{
+
+    Material[] myMaterials;
+    Material mainMaterial;
+
+	public float periodTime = 2f;
+	public float lowest = 0.5f;
+	public float highest = 0.8f;
+
+    // Use this for initialization
+    void Start()
+    {
+        myMaterials = GetComponent<MeshRenderer>().materials;
+        mainMaterial = myMaterials[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		mainMaterial.color = new Color(
+			mainMaterial.color.r,
+			mainMaterial.color.g,
+			mainMaterial.color.b,
+		 	((Mathf.Sin(Time.timeSinceLevelLoad / periodTime * Mathf.PI * 2f) + 1)/2f) * (highest - lowest) + lowest);
+    }
+}
---
+++ Assets/Scripts/TestNavmeshAgent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestNavmeshAgent : MonoBehaviour
+{
+
+    public Transform goal;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Time.frameCount % 100 == 0)
+        {
+            NavMeshAgent agent = GetComponent<NavMeshAgent>();
+            agent.SetDestination(goal.position);
+        }
+    }
+}
